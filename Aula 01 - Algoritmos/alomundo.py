2d1c465e1e2fa38703a1c4f43099f40338713fac
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-07-10 11:46:44 -0300

    Add files via upload

---
+++ Aula 01 - Algoritmos/alomundo.py
@@ -0,0 +1,12 @@
+#print("Alô mundo dos Algoritmos!")
+#print("Eu amo a Arte do Bauru \n " * 100)
+#Comentário de só uma linha
+E = 7 #Minha primeira variável
+print(type("E"))
+print(type(E))
+E = "alekao"
+print(E)
+print(type(E))
+nome = input("Digite seu nome: ")
+print("Bom dia, " + nome)
+
