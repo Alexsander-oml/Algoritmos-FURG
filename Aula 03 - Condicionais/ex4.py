de602d1b2ea1f923a95ac3d2840d95598d4f98c4
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-07-19 12:22:48 +0000

    ex4 - voto

---
+++ Aula 03 - Condicionais/ex4.py
@@ -0,0 +1,10 @@
+anoA = int(input('Digite o ano atual: '))
+anoN = int(input('Digite o ano que você nasceu: '))
+idade = (anoA-anoN)
+
+if idade >= 16 and idade <= 17 or idade >= 70:
+ print('Você tem',idade,'anos logo seu voto é facultativo!')
+if idade >= 18 and idade < 70:
+ print('Você tem',idade,'anos logo pode votar!')
+if idade < 16:
+ print('Você tem',idade,'anos logo não pode votar!')
