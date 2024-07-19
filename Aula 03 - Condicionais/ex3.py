d3dbd7d677c51ce3ed8a2f044ff74b5d0ba943e1
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-07-19 12:43:55 +0000

    Atividade 3

--- Aula 03 - Condicionais/ex3.py
+++ Aula 03 - Condicionais/ex3.py
@@ -1,10 +1,37 @@
+nome = input("Digite seu nome: ")
+hora = float(input("Digite a hora atual:"))
+if hora <= 12 and hora >= 6:
+    print("Bom dia,", nome)
+if hora > 12 and hora <= 18:
+    print("Boa tarde", nome)
+if hora > 18 and hora <= 24:
+    print("Boa noite", nome)
+if hora >= 0 and hora <= 5:
+    print("Boa madrugada", nome)
+
+
+    """
 nome = input('Digite seu nome: ')
 hora = float(input('Digite a hora atual:' ))
-if hora <= 12 and hora >= 6:
- print('Bom dia,',nome)
-if hora > 12 and hora <= 18:
- print('Boa tarde',nome)
-if hora > 18 and hora <= 24:
- print('Boa noite',nome)
-if hora >= 0 and hora <= 5:
- print('Boa madrugada',nome)
+
+if hora > 12:
+    if hora > 18:
+        saudacao = 'boa noite'
+        else:
+            saudacao = 'boa tarde'
+            else:
+                if hora > 5: 
+                saudacao = 'boa dia'
+                else:
+                    saudacao = 'boa noite'
+  print(f'{saudacao},{nome})
+
+  
+  if hora > 6 and hora < 12:
+   saudacao = 'bom dia'
+   else:
+   if hora > 12 and hora < 18:
+     saudacao = 'Boa tarde'
+    else:
+      saudacao = 'Boa noite'
+ """
