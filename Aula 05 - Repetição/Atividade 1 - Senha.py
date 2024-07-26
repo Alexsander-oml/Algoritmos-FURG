51dda04ef6bb21fc3cb12c8e92df8eef3c0e44f7
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-07-26 10:02:50 -0300

    26 - 07

--- Aula 05 - Repetição/Atividade 1 - Senha.py
+++ Aula 05 - Repetição/Atividade 1 - Senha.py
@@ -5,11 +5,14 @@ while senha != 'gragas':
 print('Senha correta')
 
 #Forma com tentativas limitadas
-
 senhaT = input('Digite senha correta: ')
 C = 4
-while C >= 0:
+while C > 0 and senhaT != 'gragas':
  if senhaT != 'gragas':
   print(f'Senha incorreta. Tentativas restantes {C}!')
   C-=1
  senha = input('Digite a senha correta: ')
+if senhaT == 'gragas':
+  print('Senha correta')
+if C == 0:
+  print('Números de tentativas zerou!') 
