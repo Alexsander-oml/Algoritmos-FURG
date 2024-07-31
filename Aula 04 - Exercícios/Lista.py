7c9fcd74f038229e1187b38c7ed62dd98bd5bddf
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-07-31 10:32:52 -0300

    Update Lista.py

--- Aula 04 - Exercícios/Lista.py
+++ Aula 04 - Exercícios/Lista.py
@@ -160,6 +160,25 @@ else:
 
 print("\n")
 
+# Modo do Celricae
+
+num1=float(input("Insira o primeiro número: "))
+num2=float(input("Insira o segundo número: "))
+num3=float(input("Insira o terceiro número: "))
+if num3>num2 and num2>num1:
+	print(num1 , num2 , num3)
+if num2>num3 and num3>num1:
+	print(num1 , num3 , num2)
+if num1>num3 and num3>num2:
+	print(num2 , num3 , num1)
+if num3>num1 and num1>num2:
+	print(num2 , num1 , num3)
+if num2>num1 and num1>num3:
+	print(num3 , num1 , num2)
+if num1>num2 and num2>num3:
+	print(num3 , num2 , num1)
+
+
 # 7. Verificar se uma data é válida (desconsiderando anos bissextos)
 print("7. Verificar se uma data é válida (desconsiderando anos bissextos)")
 dia = int(input("Digite o dia: "))
