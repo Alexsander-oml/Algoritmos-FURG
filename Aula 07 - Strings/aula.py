ba2dce18343189f18bed2439ce210ba01d58c569
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-08-18 05:06:35 +0000

    String - intro

---
+++ Aula 07 - Strings/aula.py
@@ -0,0 +1,15 @@
+#Strings -> ordem importa, texto pode ser dividido em caracteres
+
+x = 'alex santos'
+y = 'xela sotnas'
+
+x[0] = a
+x[1] = l
+x[-1] = s
+x[-3] = n
+
+len(x) = #retorna o tamanho da string
+
+x[1] = '0' # -> não pode string é imutável
+
+nome = x + ' ' + 'santos'
