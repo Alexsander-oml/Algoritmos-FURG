634ee8b16b645a0325cc5ab917c11dec2d01ed85
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-12-18 14:02:39 +0000

    bolinhas finalizadas

--- Aula15Arquivos/grafico.py
+++ Aula15Arquivos/grafico.py
@@ -1,23 +1,37 @@
-import graphics as gf # importa a biblioteca graphics e a renomeia para gf
-import random # importa a biblioteca random
+import graphics as gf  #trazer o arquivo da graphics
+import random as rd 
+import math as mt
 
-win = gf.GraphWin("Minha janela", 400, 350) # GraphWin é uma classe que cria uma janela
+def abs(x):
+    if x < 0:
+     return x * -1
+    return x
 
-c = gf.Circle(gf.Point(100,150), 10) # Circle é uma classe que cria um círculo
-cores = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white']
-c.setFill('red') # fill é um atributo que preenche o círculo com a cor especificada
-c.draw(win) # draw é um método que desenha o círculo na janela
+win = gf.GraphWin("Minha Janela", 400, 350)  # cria uma janela
+c = gf.Circle(gf.Point(100, 150), 10)  # cria o ponto
+c.setFill('red')
+c.draw(win)  # fala em qual janela o ponto deve ser draw
+cores = ['red', 'blue', 'green', 'orange', 'yellow', 'black', 'pink']
 
-# win.getMouse() # getMouse é um método que faz a janela ficar aberta até que o usuário clique em algum lugar
+bolinhas = [c]  # lista para armazenar todas as bolinhas e o ponto
+radius = 10 # raio do círculo que eh 10 pq o círculo tem 10 de raio
 
-cont = 0
 while True:
-    onde_clicou = win.getMouse() # getMouse é um método que retorna a posição do clique do usuário
-    print(onde_clicou.getX(), onde_clicou.getY()) # imprime a posição do clique do usuário
-    nova_bolinha = gf.Circle(onde_clicou, 30) # cria um novo círculo na posição do clique do usuário
-    nova_bolinha.draw(win) # desenha o novo círculo na janela
-    nova_bolinha.setFill(cores[cont]) # preenche o novo círculo com a cor da lista cores
-    cont += 1
-    if cont == len(cores):
-        cont = 0
-    nova_bolinha.draw(win) # desenha o novo círculo na janela
+     onde_cliquei = win.getMouse() # pega o ponto onde foi clicado
+     print(onde_cliquei.getX(), onde_cliquei.getY()) # mostra onde foi clicado
+     
+     for bolinha in bolinhas:
+          dx = abs(bolinha.getCenter().getX() - onde_cliquei.getX()) # diferença entre os pontos e  o centro da bolinha
+          dy = abs(bolinha.getCenter().getY() - onde_cliquei.getY()) # ||
+          d = mt.sqrt(dx * dx + dy * dy) # teorema de pitágoras
+          if d <= radius:   # se a distância for menor ou igual ao raio do círculo
+                win.close()
+                print("Fim do Programa")
+                exit() # fecha o programa
+     
+     nova_bolinha = gf.Circle(onde_cliquei, 10)
+     cor = rd.randint(0, len(cores) - 1)
+     nova_bolinha.setFill(cores[cor])
+     nova_bolinha.draw(win)
+     
+     bolinhas.append(nova_bolinha)  # adiciona a nova bolinha à lista
