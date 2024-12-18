503341ae4b175f31037daa5b266d98bbb7974fd6
Alexsander Santos <61764134+Alexsander-oml@users.noreply.github.com>
2024-12-18 14:39:49 +0000

    grafico pré finalizado

--- Aula15Arquivos/grafico.py
+++ Aula15Arquivos/grafico.py
@@ -1,6 +1,16 @@
 import graphics as gf  #trazer o arquivo da graphics
 import random as rd 
 import math as mt
+rd.seed()
+
+def foi_no_botao(botao, onde_cliquei):
+    bt_x = botao.getP1().getX()
+    bt_y = botao.getP1().getY()
+    bt2_x = botao.getP2().getX()
+    bt2_y = botao.getP2().getY()
+    print(f'----> ({bt_x},{bt_y})({bt2_x},{bt2_y})')
+    #print('Diagonal do botão: botao.getP1')
+    return True
 
 def abs(x):
     if x < 0:
@@ -16,10 +26,19 @@ cores = ['red', 'blue', 'green', 'orange', 'yellow', 'black', 'pink']
 bolinhas = [c]  # lista para armazenar todas as bolinhas e o ponto
 raio = 10 # raio do círculo que eh 10 pq o círculo tem 10 de raio
 
+botao = gf.Rectangle(gf.Point(10, 10), gf.Point(50, 30))  # cria um botão
+botao.draw(win)  # desenha o botão
+
+
 while True:
      onde_cliquei = win.getMouse() # pega o ponto onde foi clicado
      print(onde_cliquei.getX(), onde_cliquei.getY()) # mostra onde foi clicado
      
+     if foi_no_botao(botao, onde_cliquei):
+        botao.setFill('red')
+     else:
+        botao.setFill('green')
+
      for bolinha in bolinhas:
           dx = abs(bolinha.getCenter().getX() - onde_cliquei.getX()) # diferença entre os pontos e  o centro da bolinha
           dy = abs(bolinha.getCenter().getY() - onde_cliquei.getY()) # getCenter pega o centro da bolinha do getY e getX 
